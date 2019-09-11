3138cb407b08341a5e12d40546014cb272d12830
gustavaldenbratt <42805427+gustavaldenbratt@users.noreply.github.com>
2019-09-11 10:45:19 +0200

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-http.createServer(function (request, result) {
-    result.writeHead(200, {'content-type': 'text/plain'});
-    result.end('hello World');
-}).listen(8080);
