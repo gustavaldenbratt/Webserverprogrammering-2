24187b3bdc9ee22b7cc6b5f6f662e5feb16ec443
gustavaldenbratt <42805427+gustavaldenbratt@users.noreply.github.com>
2019-09-16 17:06:56 +0200

    Delete footer.php

--- Site/templates/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <h4>Footer</h4>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<a href="https://www.facebook.com" class="fa fa-facebook" target="_blank"></a>
-<a href="#" class="fa fa-instagram"></a>
-</footer>
